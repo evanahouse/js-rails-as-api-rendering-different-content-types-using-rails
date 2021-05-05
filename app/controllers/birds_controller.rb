84ec6f20f113dba462a167e3ebb944edac989713
Evan <evanahouse@gmail.com>
2021-05-05 15:12:55 -0700

    completed assignment

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,16 @@
+# class BirdsController < ApplicationController
+#   def index
+#     @birds = Bird.all
+#     render json: { birds: @birds, messages: ['Hello Birds', 'Goodbye Birds'] }
+#   end
+# end
+
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
-    render 'birds/index.html.erb'
+    birds = Bird.all
+    render json: birds
   end
 end
+
+# We don't need instance variables anymore since we are not rendering to ERB
+# instead we can just use local variables 
